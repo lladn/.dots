61c70c9b51a3996ed7df5f5ae809d203d1aa4a0e
Louie Ladiona <ladiona.louie@gmail.com>
2026-01-18 04:38:38 -0800

    uilua

---
+++ ui.lua
@@ -0,0 +1,119 @@
+return {
+
+  { "nvim-lua/plenary.nvim", lazy = true },
+  { "MunifTanjim/nui.nvim", lazy = true },
+
+  {
+    "nvim-mini/mini.icons",
+    lazy = true,
+    opts = {},
+    init = function()
+      -- From https://github.com/LazyVim/LazyVim/blob/5115b585e7df4cedb519734ffc380b7e48a366f1/lua/lazyvim/util/mini.lua
+      -- From https://github.com/LazyVim/LazyVim/blob/d35a3914bfc0c7c1000184585217d58a81f5da1a/lua/lazyvim/plugins/ui.lua#L310
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
+
+  {
+    "Bekaboo/dropbar.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" }) -- no background for dropbar
+    end,
+  },
+
+  {
+    "mikavilpas/yazi.nvim",
+    lazy = true, -- use `event = "VeryLazy"` for netrw replacement
+    keys = {
+      -- NOTE: my mapping <leader>lf is soo good but in the LSP cluster
+      {
+        "<leader>lf",
+        "<cmd>Yazi<cr>",
+        desc = "Open Yazi (file manager)",
+      },
+    },
+    opts = {
+      open_for_directories = true,
+    },
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    -- dependencies = {
+    --   { "rcarriga/nvim-notify" },
+    -- },
+    keys = {
+      { "<leader>n", "", desc = "Noice" },
+      { "<leader>nn", "<cmd>Noice all<cr>", desc = "Open Noice" },
+      { "<leader>nd", "<cmd>Noice dismiss<cr>", desc = "Dismiss Notification" },
+    },
+    opts = {
+      cmdline = {
+        view = "cmdline", -- classic cmdline at the botton
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          -- ["cmp.entry.get_documentation"] = true, requires hrsh7th/nvim-cmp
+        },
+      },
+      messages = {
+        view_search = false, -- do not show search count as virtualtext
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
+
+  {
+    "catgoose/nvim-colorizer.lua",
+    cmd = "ColorizerToggle",
+    keys = {
+      { "<leader>ux", "<cmd>ColorizerToggle<cr>", desc = "Colorizer" },
+    },
+    opts = {},
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {
+      preset = "minimal",
+      options = {
+        show_source = {
+          enabled = true,
+        },
+        multilines = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("tokyonight").setup()
+      vim.cmd("colorscheme tokyonight-moon")
+    end,
+  },
+
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "#d3cdc3",
+    },
+  },
+}
